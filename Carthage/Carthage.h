ccf6bb6ce1c7ef6662a5f963fd0da74c01793ef4
wuhaizeng <wuhaizeng@cmss.chinamobile.com>
2017-11-28 17:39:43 +0800

    add carthage

---
+++ Carthage/Carthage.h
@@ -0,0 +1,19 @@
+//
+//  Carthage.h
+//  Carthage
+//
+//  Created by wuhaizeng on 28/11/2017.
+//  Copyright © 2017 www.skyfox.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Carthage.
+FOUNDATION_EXPORT double CarthageVersionNumber;
+
+//! Project version string for Carthage.
+FOUNDATION_EXPORT const unsigned char CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carthage/PublicHeader.h>
+
+
